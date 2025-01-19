08429dec1271a131ceb3759e53902233017644fa
vikaspal9131 <vikaspal968562@gmail.com>
2025-01-19 21:40:51 +0530

    fix image remove problem

--- QRity/script.js
+++ QRity/script.js
@@ -3,6 +3,12 @@ const input = document.querySelector("#url");
 const image = document.querySelector("#qr-img")
 
 btn.addEventListener("click" , () =>{
-  const url = input.value;
-  image.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${url}`
+
+    const url = input.value;
+    if(url == ''){
+      alert('Please enter your URL')
+    }
+    else{
+      image.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${url}`
+    }
 })
